af4939b6433a42d85053b4ff7f47f800248a7170
misterbanhman <nathan.i.banh@gmail.com>
2022-11-26 21:20:54 -0700

    Change class and updated methods. Not quite finished yet.

--- main.rb
+++ main.rb
@@ -1,125 +1,81 @@
-module GameBoard
-    array = [*1..9]
-    board = "_1_|_2_|_3_\n_4_|_5_|_6_\n_7_|_8_|_9_"
+class GameBoard
+  @@array = [1*..9]
+  @@board = "_1_|_2_|_3_\n_4_|_5_|_6_\n_7_|_8_|_9_"
+  @@p1_win_condition = 0
+  @@p2_win_condition = 0
 
-    def update_array(index))
-      array.delete_at(inex)
-    end
-
-    def update_board(number,player)
-      choice = array.index(number)
-      board = board.gsub!(choice,player)
-      update_array(choice)
-      return board
-    end
-
-  end
-  
-  
-  
-  
-  class Player
-    include GameBoard
-
-    def initialize(name, win_condition)
-      @name = name
-      @win_condition = win_condition
-    end
-  
-    def player_choice
-      puts "Where would you like to place your piece?"
-      player = gets.chomp
-      return player
-    end
-
-    def computer_choice
-      computer = array.sample
-      update_array(computer)
-      return computer
-    end
-
-    def check_win
-      if array[index+1] == player && array[index+2] == player
-        set win == true
-  
-      elsif array[index+3] == player && array[index+3] == player
-        set win == true
-  
-      elsif array[index+4] == player && array[index+4] == player
-        set win == true
-  
-      else
-  
-      end
-    end
-
-    def simulate_game
-      while @win_condition == 0 
-        self.update_board(self.player_choice, 'X')
-
-      end
-    end
-
-  end
-  
-  # puts "What is your name?"
-  # query = gets.chomp
-  # first_player = Player.new(query)
-  # puts first_player.print_name
-  
-puts "Hello there! What is your name?"
-get_name = gets.chomp 
-
-new_player = Player.new(name, 0)
-
-
-
-
-
-  board = "_1_|_2_|_3_\n_4_|_5_|_6_\n_7_|_8_|_9_"
-  # puts board
-  array = [*1..9]
-  
-  while array.length > 0
-    computer_choice = array.sample
-    puts computer_choice
-    puts array.index(computer_choice)
-  
-    board = board.gsub(computer_choice.to_s,'X')
-    puts "\n" + board + "\n"
-  
-    array.delete_at(array.index(computer_choice))
-    p array
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
   end
 
-  [1,2,3,4,5,..9]
-  [X,2,3,X,5,6,X,8,9]
+  def update_array(number, symbol)
+    @@array.map {|x| x == number? symbol : x}
+  end
 
-  array.each do |spot|
-    if spot == 'x'
-      check_win(array.index(spot), spot)
+  def update_board(number, player)
+    if player == @player1
+      update_array(number, 'X')
+      board.gsub!(number.to_s, 'X')
+      puts board
+      check_win(player)
+    end
 
-    elsif spot == 'y'
-      check_win(array.index(spot), spot)
-
-    else
-
+    else player == @@player2
+      update_array(number, 'O')
+      board.gsub!(number.to_s, 'O')
+      puts board
+      check_win(player)
     end
   end
 
+  def check_win(player)
+    if @@array[index+1] == player && @@array[index+2] == player
+      set_win(player)
 
-  def check_win(index, player)
-    if array[index+1] == player && array[index+2] == player
-      set win == true
+    elsif @@array[index+3] == player && @@array[index+3] == player
+      set_win(player)
 
-    elsif array[index+3] == player && array[index+3] == player
-      set win == true
-
-    elsif array[index+4] == player && array[index+4] == player
-      set win == true
-
-    else
-
+    elsif @@array[index+4] == player && @@array[index+4] == player
+      set_win(player)
     end
-
   end
+
+  def check_valid_number(number)
+    
+  end
+
+  def set_win(player)
+    if player == @player1
+      @@p1_win_condition = 1
+    else 
+      @@p2_win_condition = 1
+    end
+  end
+
+  def simulate_game
+    puts "This will be a standard 1v1 game of Tic-Tac-Toe. \n You will be 'Player 1' and using symbol 'X'. What is your name?"
+    @player1 = gets.chomp
+    player1 = Player.new(@player1 'X')
+
+    puts "You will be Player 2. What is your name?"
+    player2 = gets.chomp
+    player2 = Player.new(@player2, 'O')
+
+    while @@p1_win_condition == 0 || @@p2_win_condition = 0
+      puts "Here is the gameboard, please choose your position. \n #{@@board}"
+      player1_move = gets.chomp 
+end
+
+class Player
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  
+  def player_choice
+    puts "Where would you like to place your piece?"
+    player = gets.chomp
+    return player
+  end
+
+end
